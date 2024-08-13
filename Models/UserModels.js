42ec35c267069f3757ca4d7f2f61a51bbab97239
viejov3 <yugandharselvam@gmi=ail.com>
2024-08-13 11:38:26 +0530

    models

---
+++ Models/UserModels.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        require:true,
+    },
+    email:{
+        type:String,
+        require:true,
+        unique: true,
+    },
+    phoneNumber:{
+        type:String,
+        require:true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        require:true,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+
+});
+
+const UserDB = mongoose.model("NewUser", userSchema);
+export default UserDB;
