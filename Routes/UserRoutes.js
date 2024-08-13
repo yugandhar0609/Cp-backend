886bdecab8179ab83798b08c4c54d4f63ad0db7c
viejov3 <yugandharselvam@gmi=ail.com>
2024-08-13 14:11:43 +0530

    commit

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import Register from '../Controllers/Register.js'
+import login from '../Controllers/Login.js'
 
 const authRouter = express.Router();
 
 authRouter.post('/register',Register);
+authRouter.post('/login',login);
 
 export default authRouter;
