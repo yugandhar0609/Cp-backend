aeb89f9e6157d0a8efb6c5a85824e7a852111a91
Ganganatarajan <ganganatarajan2000@gmail.com>
2024-08-13 16:44:47 +0530

    form

--- Controllers/Form.js
+++ Controllers/Form.js
@@ -1,17 +1,34 @@
 import FormDB from '../Models/FormModels.js'
+import nodemailer from 'nodemailer'
 
 const PostForm = async(req,res)=>{
 try {
     const {St_name,St_email,St_phoneNumber,St_state,St_country} = req.body;
     const formdata = new FormDB({St_name,St_email,St_phoneNumber,St_state,St_country});
     await formdata.save();
-    return res.status(200).json({messege:"posted sucessfully...."})
+
+    const mail=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'yugandhardeveloper@gmail.com',
+            pass:'xcms leoa dyol rxsy'
+        }
+
+    })
+
+    const options={
+        Form:'yugandhardeveloper@gmail.com',
+        To:'St_email',
+        Subject:'Form Submission',
+        Text:`Hello,${St_name},\n\n Thank you for submitting the form`,
+    }
+    await mail.sendMail(options)
+    return res.status(200).json({messege:"form posted and email send sucessfully...."})
     
 } catch (error) {
-    return res.status(401).json({messege:"error in postForm"})
+    return res.status(401).json({messege:"error in postForm or sending email"})
 }
 
-   
 }
 
 export default PostForm;
